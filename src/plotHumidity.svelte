aafb729c114044fd53eaa07087482f5d23da990f
Toni Cafiero <tcafiero@hotmail.com>
2020-11-14 15:57:01 +0100

    work in progress

--- src/plotHumidity.svelte
+++ src/plotHumidity.svelte
@@ -1,7 +1,7 @@
 <script>
 	// Create a client instance
-	var wsbroker = "3.122.235.37";  //mqtt websocket enabled broker
-	var wsport = 9001 // port for above
+	var wsbroker = "m21.cloudmqtt.com";  //mqtt websocket enabled broker
+	var wsport = 32605 // port for above
 	var client = new Paho.MQTT.Client(wsbroker, wsport, "myclientid_" + parseInt(Math.random() * 100, 10));
 	
 	// set callback handlers
